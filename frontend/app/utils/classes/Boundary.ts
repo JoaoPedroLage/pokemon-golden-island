219fadeceac9d3a11d739a965155be9335b5170b
João Pedro Lage <JoaoPedro.Lage@outlook.com>
2025-11-13 19:46:26 -0300

    refactor: translate comments to English

--- frontend/app/utils/classes/Boundary.ts
+++ frontend/app/utils/classes/Boundary.ts
@@ -1,9 +1,9 @@
 import { Position } from '../../interfaces/mainInterface';
 
 export class Boundary {
-  position: Position; // Posição do Boundary
-  width: number;      // Largura do Boundary
-  height: number;     // Altura do Boundary
+  position: Position; // Boundary position
+  width: number;      // Boundary width
+  height: number;     // Boundary height
 
   constructor({ width, height, position }: { position: Position, width: number, height: number }) {
     this.position = position;
@@ -12,16 +12,16 @@ export class Boundary {
   }
 
   draw(ctx: CanvasRenderingContext2D): void {
-    ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'; // Cor de visualização para a colisão
+    ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'; // Visualization color for collision
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
-  // Método para verificar colisões
+  // Method to check collisions
   static checkCollision(rectA: Boundary, rectB: Boundary): boolean {
     return (
-      rectA.position.x < rectB.position.x + rectB.width / 2 && // Verifica a colisão na metade da largura pois o personagem é mais largo
+      rectA.position.x < rectB.position.x + rectB.width / 2 && // Check collision at half width since character is wider
       rectA.position.x + rectA.width > rectB.position.x &&
-      rectA.position.y < rectB.position.y + rectB.height / 2 && // Verifica a colisão na metade da altura pois o personagem é mais alto
+      rectA.position.y < rectB.position.y + rectB.height / 2 && // Check collision at half height since character is taller
       rectA.position.y + rectA.height > rectB.position.y
     );
   }
